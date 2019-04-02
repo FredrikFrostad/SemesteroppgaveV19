ba2ccdeb71eb5737aebb2dc2206b5161449269d0
FredrikFrostad <frostad.fredrik@gmail.com>
2019-04-02 13:02:20 +0200

    Lagt til ny controller eog metode for å bytte scene. Har problemer med resourcepath. Er heller ikke fornøyd med metode for scenebytte. Det er hackete, finnes sikkert en bedre løsning.

--- src/main/java/com/programutvikling/controller/MainFXMLController.java
+++ src/main/java/com/programutvikling/controller/MainFXMLController.java
@@ -3,8 +3,16 @@ package com.programutvikling.controller;
 import com.programutvikling.models.osType.OSType;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
 import java.io.File;
+import java.io.IOException;
+
 
 public class MainFXMLController {
 
@@ -38,8 +46,20 @@ public class MainFXMLController {
 
     @FXML
     private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Morra di er mann!");
+        try {
+            Parent scene2Parent = FXMLLoader.load(getClass().getResource("controller/view2Example.fxml"));
+            Scene scene2 = new Scene(scene2Parent);
+
+            Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+            window.setScene(scene2);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+            System.out.println("FØØØKKKK JAVA PATH HÆLVETTE");
+        }
+
     }
 
 
