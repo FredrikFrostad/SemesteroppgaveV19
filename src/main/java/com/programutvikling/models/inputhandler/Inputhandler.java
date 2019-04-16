883992a7d56fa534614918e534289a9147c0b61e
fredrikfrostad <frostad.fredrik@gmail.com>
2019-04-16 09:33:44 +0200

    Lagt til metode for validering av navn

--- src/main/java/com/programutvikling/models/inputhandler/Inputhandler.java
+++ src/main/java/com/programutvikling/models/inputhandler/Inputhandler.java
@@ -22,10 +22,27 @@ public class Inputhandler {
         return true;
     }
 
+    /**
+     * Method for validating the formatting of names
+     * @param name String containing the name to be verified
+     * @return true if the parameter String passes all checks
+     * @throws InvalidNameFormatException Exception is thrown when the parameter String is illegally formatted
+     */
     public static boolean checkValidNameFormat(String name) throws InvalidNameFormatException {
+        if (name.length() < 2) {
+            throw new InvalidNameFormatException("Names need to be at least 2 characters in length");
+        }
         if (name.matches("[0-9]")) {
             throw new InvalidNameFormatException("Numbers or special characters are not allowed here");
         }
+        if (name.length() > 25) {
+            throw new InvalidNameFormatException("Names cannot exceed 25 characters in length");
+        }
+        /* TODO: Fjern denne iffen hvis den ikke trengs
+        if (!name.matches("[a-åA-Å]]")) {
+            throw new InvalidNameFormatException("Pleace enter a valid Name");
+        }
+         */
 
         return true;
     }
