adeb0358e30899afaf5453ac32337cb4b75f34fc
fredrikfrostad <frostad.fredrik@gmail.com>
2019-04-17 22:35:40 +0200

    la til javadoc comment for å forklare mainappwrppers eksistens

--- src/main/java/com/programutvikling/mainapp/MainApp.java
+++ src/main/java/com/programutvikling/mainapp/MainApp.java
@@ -121,7 +121,6 @@ public class MainApp extends Application {
                     }
                 }
             }
-
         }
     }
 
--- src/main/java/com/programutvikling/mainapp/MainAppWrapper.java
+++ src/main/java/com/programutvikling/mainapp/MainAppWrapper.java
@@ -1,7 +1,11 @@
 package com.programutvikling.mainapp;
 
+/**
+ * This class exists only as a workaround to what appears to be a maven bug, where execution of a built
+ * jar file fails if the mainClass defined in mavens pom.xml file is the class extending javafxs Application class
+ */
 public class MainAppWrapper {
-
+    
     public static void main(String[] args) {
         MainApp.main(args);
     }
