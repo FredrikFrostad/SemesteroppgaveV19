a963736b4db978339b4861461a234e9a69cbbcf7
fredrikfrostad <frostad.fredrik@gmail.com>
2019-04-17 16:05:49 +0200

    Implementert enkel csvFilereader

--- src/main/java/com/programutvikling/models/filehandlers/reader/CsvReader.java
+++ src/main/java/com/programutvikling/models/filehandlers/reader/CsvReader.java
@@ -1,20 +1,57 @@
 package com.programutvikling.models.filehandlers.reader;
 
+import com.programutvikling.mainapp.MainApp;
+import com.programutvikling.models.data.kunde.Kunde;
 import javafx.stage.FileChooser;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class CsvReader extends FileReader{
 
     @Override
     public Object readDataFromFile(File file) throws Exception {
-        return null;
+        BufferedReader reader = null;
+        List<Object> objLst = new ArrayList<>();
+
+        try {
+            String line = "";
+            reader = new BufferedReader(new java.io.FileReader(file));
+            reader.readLine();
+
+            while ((line = reader.readLine()) != null) {
+                String[] dataFields = line.split(",");
+                objLst.add(dataFields);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return objLst;
     }
 
     @Override
     public Object readDataFromURL(String url) {
         return null;
+    }
+
+    public static void main(String[] args) {
+
+        try {
+            ArrayList<String[]> list = (ArrayList<String[]>) new CsvReader().readDataFromFile(new File("testfile.csv"));
+            for (String[] arr : list) {
+                for (String s : arr) {
+                    System.out.print(s);
+                }
+                System.out.println();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
