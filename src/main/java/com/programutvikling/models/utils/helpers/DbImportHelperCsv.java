3eacf707c609cd68373de38e644ef98d59b5aea9
fredrikfrostad <frostad.fredrik@gmail.com>
2019-04-30 15:33:27 +0200

    Lese og skrive store datasett (1M elementer) til csv fungerer, skrive store datasett til jobj fungerer, lese store datasett fra jobj fungerer ikke særlig bra.

--- src/main/java/com/programutvikling/models/utils/helpers/DbImportHelperCsv.java
+++ src/main/java/com/programutvikling/models/utils/helpers/DbImportHelperCsv.java
@@ -23,12 +23,12 @@ public class DbImportHelperCsv{
         } else {
             filePath = path;
         }
-        System.out.println("Printfilepath 1 " + filePath);
+
         File[] dbFiles = new File(filePath).listFiles();
-        System.out.println("Antall filer: " + dbFiles.length);
         ArrayList<Kunde> clientList = MainApp.getClientList();
         ArrayList<String[]> policyList;
         CsvReader reader = new CsvReader();
+        CsvObjectBuilder builder = new CsvObjectBuilder();
 
         Arrays.sort(dbFiles);
 
@@ -36,11 +36,23 @@ public class DbImportHelperCsv{
             System.out.println(file.getAbsolutePath());
             try {
                 if (file.getName().equals("clients.csv")) {
+                    boolean startUp = false;
+                    if (MainApp.getClientList().isEmpty()) {
+                        startUp = true;
+                    }
                     ArrayList<String[]> list = reader.readDataFromFile(new File(file.getAbsolutePath()));
+
+                    // Building customer object from string and adding them to list.
                     for (String[] s : list) {
-                        Kunde k = (Kunde) new CsvObjectBuilder().buildObjectFromString(s);
-                        if (!clientListContains(clientList, k)) {
-                            clientList.add((Kunde) new CsvObjectBuilder().buildObjectFromString(s));
+                        Kunde k = (Kunde) builder.buildObjectFromString(s);
+
+                        // This procedure is horribly slow when adding large datasets.
+                        // Using startUp flag to skip duplicate check when populating clientobject arraylist
+                        // on program startup
+                        if (startUp) {
+                            clientList.add((Kunde) builder.buildObjectFromString(s));
+                        } else if (!clientListContains(clientList, k)) {
+                            clientList.add((Kunde) builder.buildObjectFromString(s));
                         }
                     }
                 } else {
@@ -58,19 +70,19 @@ public class DbImportHelperCsv{
 
                     }
                 }
-
             } catch (IOException e) {
                 e.printStackTrace();
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
-
     }
 
     /**
      * Method for checking if an object is already loaded. This method works on
      * the premise no two client objects can have the same policynumber
+     * This method is usually called in a loop, and is obviously very slow. If the parameter list
+     * is sorted this can be done using binary search to greatly improve performance. 
      * @return true if the object is in the list
      */
     private boolean clientListContains(ArrayList<Kunde> list, Kunde client) {
